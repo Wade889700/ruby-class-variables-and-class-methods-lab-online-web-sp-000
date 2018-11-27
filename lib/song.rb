f26afed8ac3d960c9440e99d5c9dd0f2e7cab45c
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-27 21:37:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,4 +41,16 @@ class Song
   end
 
 
+  def self.artist_count
+    genre_hash={}
+    @@genres.each do |genre|
+      if genre_hash[genre]
+        genre_hash[genre] +=1
+      else
+        genre_hash[genre] = 1
+    end
+    end
+    genre_hash
+  end
+
 end
